d3f0c4d126f50cf410fd4d872361781388af7300
Guillaume Maiano <guillaume@maiano.fr>
2020-07-29 09:15:00 +0100

    I find go 'receivers' to resemble Swift protocols

--- LanguageFeatures/Receivers/deck.go
+++ LanguageFeatures/Receivers/deck.go
@@ -6,6 +6,7 @@ type deck []string
 
 // functions know what they work on based on the parameter defined after the parenthesis
 func(d deck) print() {
+	// by convention, go-fluent writers define the reference for the type of the receiver function with a one or two letter word (here, d, like deck)
 	for i,c := range d {
 	 fmt.Println(i, c)
 	}
