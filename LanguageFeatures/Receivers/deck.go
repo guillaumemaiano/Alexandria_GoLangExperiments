dfaba3415bbfda64e4cc679bf41c12ef1053227f
Guillaume Maiano <guillaume@maiano.fr>
2020-07-29 09:25:00 +0100

    Fixes the import in deck.go (side effect of removal in main.go), adds a method for generating a 52-card deck

--- LanguageFeatures/Receivers/deck.go
+++ LanguageFeatures/Receivers/deck.go
@@ -1,8 +1,26 @@
 package main
 
+import "fmt"
+
 // create a  new type called deck, using the slices we learned
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	// cards are a combination of values in a given suit - which is really a bad English transliteration of 'suite', because they 'follow' each other, but whatever.
+	cardSuits := []string { "Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Knave", "Lady", "King"}
+
+	// mix and match
+	for _, suit := range  cardSuits {
+		for  _, value := range cardValues {
+			cards = append(cards, suit + " of " + value)
+		}
+	}
+	return cards
+}
 
 // functions know what they work on based on the parameter defined after the parenthesis
 func(d deck) print() {
